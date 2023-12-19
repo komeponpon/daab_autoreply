a884105aa991901f8a1cf39c24cb3902a699479a
komeponpon <ricemountain1030@gmail.com>
2023-12-19 15:58:59 +0900

    モジュールの統合

--- scripts/reply.js
+++ scripts/reply.js
@@ -1,12 +1,33 @@
 const holidayJp = require('@holiday-jp/holiday_jp');
 
-// 週末や祝日にメッセージに応答するモジュール
 module.exports = function(robot) {
+  // メッセージが週末や祝日に受信された場合の処理
   robot.hear(/.*/, function(msg) {
     // メッセージが週末や祝日に受信されているか確認
     if (isWeekendOrHoliday()) {
       msg.reply('いつも大変お世話になっております。\n本日公休日となりますので、\n翌営業日に担当よりご連絡致します。');
     }
+
+    // 最後のメッセージの情報を更新
+    lastMessageText = msg.message.text;
+    lastMessageTime = new Date().getTime();
+
+    // タスクが完了していない場合の処理
+    if (!taskCompleted) {
+      msg.send({
+        title: lastMessageText,
+        closing_type: 1
+      });
+      // 完了を管理
+      taskCompleted = true;
+    }
+
+    // 2分後に自動メッセージを送信
+    setTimeout(function() {
+      if (new Date().getTime() - lastMessageTime > 120000 && !taskCompleted) {
+        msg.send('いつも大変お世話になっております。\n本日公休日となりますので、\n翌営業日に担当よりご連絡致します。');
+      }
+    }, 120000);
   });
 
   function isWeekendOrHoliday() {
@@ -20,49 +41,13 @@ module.exports = function(robot) {
 
     // 祝日かどうかをチェック
     const holidays = holidayJp.between(currentDate, currentDate);
-    if (holidays.length > 0) {
-      return true;
-    }
-
-    return false;
+    return holidays.length > 0;
   }
-};
 
-// メッセージを受信してから2分後に自動応答するモジュール
-let lastMessageTime = 0;
-let lastMessageText = '';
+  // 最後のメッセージの情報を保持する変数
+  let lastMessageTime = 0;
+  let lastMessageText = '';
 
-module.exports = function(robot) {
-  robot.hear(/.*/, function(msg) {
-    const currentTime = new Date().getTime();
-
-    // 最後に受け取ったメッセージの情報を更新
-    lastMessageText = msg.message.text;
-    lastMessageTime = currentTime;
-
-    // 2分経過しているかを確認
-    if (currentTime - lastMessageTime > 120000) {
-      // 2分後に自動メッセージを送信
-      setTimeout(function() {
-        msg.send('いつも大変お世話になっております。\n本日公休日となりますので、\n翌営業日に担当よりご連絡致します。');
-      }, 120000);
-    }
-  });
-
-  // タスクのタイトルとして最後のメッセージのテキストを送信
+  // タスクの完了状態を管理する変数
   let taskCompleted = false;
-
-  robot.respond(/.*/, function(res){
-    //タスクが完了していない場合の処理
-    if(!taskCompleted){
-      res.send({
-        title: `${lastMessageText}`,
-        closing_type: 1
-      });
-      //完了を管理
-      taskCompleted = true;
-    }
-    //完了している場合何も返さない
-  });
-
-}
+};
