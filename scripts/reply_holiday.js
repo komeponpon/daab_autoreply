57752396434e0ba8b8f22f8bdb369327abbfe524
komeponpon <ricemountain1030@gmail.com>
2024-02-22 14:14:45 +0900

    試験運用

--- scripts/reply_holiday.js
+++ scripts/reply_holiday.js
@@ -2,29 +2,21 @@ const holidayJp = require('@holiday-jp/holiday_jp');
 
 module.exports = function(robot) {
 
-
-  // メッセージが受信されたときの処理
-  robot.hear(/.*/, function(msg){
-    // 受信したメッセージの情報を保存
-    lastMessageText = msg.message.text;
-    lastMessageTime = new Date().getTime();
-  });
-
-  //受信したメッセージの情報を定義
+  //受信したメッセージを定義
   let lastMessageText = '';
   let lastMessageTime = 0;
 
-  //連投フラグ
-  let isProcessing = false;
+  //組織IDの定義
+  const desiredDomain = '_94963722_822083584';
 
-  //公休日かどうか？
+  //公休日かどうかの判定
   function isWeekendOrHoliday(){
 
     //現在の日付を取得
     const currentDate = new Date();
 
-    //土日かどうかをチェック(日0 土6)
-    if (currentDate.getDay() === 2 || currentDate.getDay() === 6){
+    //土日かどうかをチェック（日0 土6）
+    if (currentDate.getDay() === 4 || currentDate.getDay === 6){
       return true;
     }
 
@@ -33,29 +25,42 @@ module.exports = function(robot) {
     return holidays.length > 0;
   }
 
+  //連投フラグ
+  let isProcessing = false;
 
-  //n分後に返信処理
+
+  //メッセージが受信された時の処理
   robot.hear(/.*/, function(msg){
-    if(isWeekendOrHoliday() && !isProcessing) {
+
+
+    //受信したメッセージ情報の保存
+    lastMessageText = msg.message.text;
+    lastMessageTime = new Date().getTime();
+
+    //受信したメッセージの組織IDを取得
+    messageDomain = msg.message.rooms[msg.message.room].domain.id;
+    console.log(messageDomain);
+
+    //公休日かつ組織IDが一致する場合の処理
+    if(isWeekendOrHoliday() && messageDomain === desiredDomain && !isProcessing){
       //連投フラグを設定
       isProcessing = true;
-        setTimeout(function(){
-            // 某ルームIDの配列
-            const rooms = ['_95069676_-914358272'];
-            // 某ルーム以外を定義
-            const filteredRooms = rooms.filter(roomId => roomId !== msg.message.room);
-
-            // 某以外のルームには返信処理
-            filteredRooms.forEach(room =>{
-                msg.reply('いつも大変お世話になっております。\n本日公休日となりますので、\n翌営業日に担当よりご連絡致します。');
-                robot.send({room: room},{
-                    title: lastMessageText,
-                    closing_type: 0
-                });
-            });
-      //処理が完了したら連投フラグを解除
+      setTimeout(function(){
+        //何某ルームIDの配列（タスクアップ先）
+        const rooms = ['_95069676_-914358272'];
+        //何某ルーム以外を定義
+        const filteredRooms = rooms.filter(roomId => roomId !== msg.message.room);
+        //何某ルーム以外には返信処理
+        filteredRooms.forEach(room =>{
+          msg.reply('いつも大変お世話になっております。\n本日公休日となりますので、\n翌営業日に担当よりご連絡致します。');
+          robot.send({room: room},{
+            title: lastMessageText,
+            closing_type: 0
+          });
+        });
+      //処理が完了したらフラグを解除
       isProcessing = false;
-        }, 0.05 * 60 * 1000); // 処理時間設定用
+      }, 0.05 * 60 * 1000); //処理時間を設定(テストのため3秒)
     }
-});
+  });
 }
--- scripts/room_info.js
+++ scripts/room_info.js
@@ -1,6 +1,12 @@
-module.exports = (robot) => {
-  robot.hear(/.*/, (res) => {
+//module.exports = (robot) => {
+  //robot.hear(/.*/, (res) => {
     //トークルームのIDを確認
     //res.send(`This room id is ${res.message.room}`);
-  });
-}
+  //});
+
+  //デバッグコンソールへ組織IDを表示
+  //robot.hear(/.*/, () => {
+    //const messageDomain = res.message.rooms[res.message.room].domain.id;
+    //console.log(messageDomain);
+  //});
+//}
